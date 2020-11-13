7c23f043e28ac7511011f397e5c0ca30e8d64b96
atulverma-git <atulverma.515@gmail.com>
2020-11-13 21:18:32 +0530

    1st commit

--- src/com/java8/basic/BasicJavaTest.java
+++ src/com/java8/basic/BasicJavaTest.java
@@ -4,11 +4,20 @@ public class BasicJavaTest {
 	public static void main(String[] arg){
 		Integer i = 10;
 		int j = 10;
-		System.out.println(i==j);
+
+		System.out.println("i==j : "+(i==j));
+
+		System.out.println("i.equals(j) : "+i.equals(j));
 		
-		String s = "hello";
-		String s1 = new String("hello");
+		String s1 = "Hello World";
+		String s2 = new String("Hello World");
+		String s3 = "World";
 		
-		System.out.println(s==s1);
+		System.out.println("s1==s2 : "+ (s1==s2));
+		System.out.println("s1.equals(s2) : "+ s1.equals(s2));
+		System.out.println(s1.substring(6));
+		System.out.println(""+ (s3.equals(s1.substring(6))));
+
+
 	}
 }
